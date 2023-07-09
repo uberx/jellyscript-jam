8ff2f9f90c0c0fd08e1eb6b93f2014d64fc12080
varangian-core <96369233+varangian-core@users.noreply.github.com>
2023-07-09 14:15:08 -0700

    Built the proto app (guess the number), created the component test, changes tsconfiguration to be compatible with the testing suite.

--- front-end-jellyscript/src/App.test.tsx
+++ front-end-jellyscript/src/App.test.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the Guess the Number heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Guess the Number/i);
+  expect(headingElement).toBeInTheDocument();
 });
