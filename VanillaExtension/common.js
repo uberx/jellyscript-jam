d6578d7a2c18912285db1d8165644f836cd8b42f
Muhammet Can <07finikeli07143@gmail.com>
2023-07-11 09:12:00 +0300

    PanelView Logic

--- VanillaExtension/common.js
+++ VanillaExtension/common.js
@@ -1,1 +1,14 @@
 /* Will be adding code here soon */
+function updateOptions() {
+    // Loads elements of the array into optionA dropdown menu
+    $('#selectA').empty();
+    $.each(options, function(i, p) {
+      $('#selectA').append($('<option></option>').val(p).html(p));
+    });
+    
+    // Loads elements of the array into optionB dropdown menu
+    $('#selectB').empty();
+    $.each(options, function(i, p) {
+      $('#selectB').append($('<option></option>').val(p).html(p));
+    });
+  }
