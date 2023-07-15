ad6eb87023c851f4da73a35a7c83824f15cfa1ab
uberx <anirudhroxrulz@gmail.com>
2023-07-15 01:52:36 -0700

    [frontend] Commenting out spammy console.log statements

--- frontend/dino-game/config.js
+++ frontend/dino-game/config.js
@@ -5,7 +5,7 @@ const twitch = window.Twitch.ext;
 
 // onContext callback called when context of an extension is fired 
 twitch.onContext((context) => {
-  console.log(context);
+  //console.log(context);
 });
 
 
